aaaf270d9550ebdd5081d2e2af59f615d95725bf
Iari Manfredi <byrillum@gmail.com>
2020-03-25 17:13:47 +0100

    Modified file main.py: about 5 more lines

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
+def tree(n):
+    for j in range(n+1):
+        print('{}'.format(mess), end = ' ')
+    print('\n')
+
 print('Scrivi il messaggio da stampare e quante volte lo vuoi stampare\n')
 mess = input('Messaggio --> ')
 t = int(input('Numero --> '))
 for i in range(t):
-    print('{}\n'.format(mess))
+    tree(i)
 
